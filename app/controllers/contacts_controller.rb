a4e0322ba268ed4280374a367c83886667e191ce
Anthony Wu <anthonylongwu19@gmail.com>
2015-11-12 20:17:52 -0800

    made changes, including form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,21 @@ class ContactsController < ApplicationController
   def index
     @all_contacts = Contact.all
   end
+
+  def submit
+    # @all_contacts = Contact.all
+    # contact = params[:contact]
+    # @all_contacts << contact 
+  end
+
+  def result
+    # @all_contacts = Contact.all
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    email = params[:email]
+    phone_number = params[:phone_number]
+    Contact.create(first_name: first_name, last_name: last_name, email: email, phone_number: phone_number)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/contacts', to: 'contacts#index'
+  get '/submit', to: 'contacts#submit'
+  post '/submit', to: 'contacts#submit'
+  post '/result', to: 'contacts#result'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
